a1e0d8a1ca4506093d1e0d5292dcea646b9af760
Nikolay Danilenko <kkt1337@gmail.com>
2022-05-04 00:14:51 +0300

    dev

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,35 @@
 import java.io.File;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
 
-        String folderPath = "C:/Users/79652/Desktop/Скрины для дом работы";
+
+        String folderPath = "C:/Program files (x86)/Steam";
         File file = new File(folderPath);
 
-        System.out.println(getFolderSize(file));
+        long start = System.currentTimeMillis();
+        FolderSizeCalculator calculator =
+                new FolderSizeCalculator(file);
+
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+
+        long duration = (System.currentTimeMillis() - start);
+        System.out.println(duration + " seconds");
 
 
     }
-    public static long getFolderSize(File folder){
-        if(folder.isFile()){
+
+    public static long getFolderSize(File folder) {
+        if (folder.isFile()) {
             return folder.length();
         }
         long sum = 0;
-        File [] files =folder.listFiles();
-        for(File file : files){
-            sum+=getFolderSize(file);
+        File[] files = folder.listFiles();
+        for (File file : files) {
+            sum += getFolderSize(file);
 
         }
         return sum;
